61d87c072c1ab6cee63b1b54363c5b22915f6750
Elena_L <ledenewaen@gmail.com>
2024-06-22 20:18:48 +0700

    new module procesing

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
-from masks import get_mask_account, get_mask_card_number
+from widget import get_data, mask_account_card
+from processing import sort_by_date
 
-print(get_mask_card_number("7000792289606361"))
-print(get_mask_account("73654108430135874305"))
+list_id = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+print(mask_account_card("Счет 73654108430135874305"))
+print(get_data("2018-07-11T02:26:18.671407"))
+print(sort_by_date(list_id))
