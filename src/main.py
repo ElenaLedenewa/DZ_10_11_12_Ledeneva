4e2cc6a719244cb588820f8248035c1b3f3efdf4
LedenevaEN <ledenewaen@gmail.com>
2024-06-28 22:19:14 +0700

    edit feature_task1 main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-# Работа с виджетами
+# Работа с виджетами клиента
 from widget import get_data, mask_account_card  # type: ignore
 from processing import sort_by_date, filter_by_state  # type: ignore
 
